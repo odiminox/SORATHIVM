6e63225fd260a41441a5f844b6d9d14e14795072
Simon Jordan <simon@thepolygonloft.com>
2022-04-19 08:19:28 +0100

    more vector maths

--- blueprint/blueprint/source/common.h
+++ blueprint/blueprint/source/common.h
@@ -1,3 +1,7 @@
 #pragma once
 
 #include "types.h"
+
+#define PI 3.141592653589f
+
+#define DEG2RAD(deg) ((deg)*PI/180.0f)
--- blueprint/blueprint/source/math.h
+++ blueprint/blueprint/source/math.h
@@ -1,10 +1,9 @@
 #pragma once
 
+// TODO: Remove maths dependencies
+#include <cmath>
+
 #include "common.h"
-
-#define PI 3.141592653589f
-
-#define DEG2RAD(deg) ((deg)*PI/180.0f)
 
 union v2
 {
@@ -17,6 +16,12 @@ FORCEINLINE v2 operator -(const v2& a, const v2& b) { return { a.x - b.x, a.y - 
 FORCEINLINE v2 operator *(const v2& a, const f32 s) { return { a.x * s, a.y * s }; }
 FORCEINLINE v2 operator /(const v2& a, f32 s) { s = 1.0f / s; return { a.x / s, a.y / s }; }
 FORCEINLINE v2 operator -(const v2& a) { return { -a.x, -a.y }; }
+FORCEINLINE bool operator ==(const v2& a, const v2& b) { a.x == b.x && a.y == b.y; }
+FORCEINLINE bool operator !=(const v2& a, const v2& b) { a.x != b.x || a.y != b.y; }
+
+FORCEINLINE f32 magnitude(const v2& v) { return sqrt((v.x * v.x) + (v.y * v.y)); }
+FORCEINLINE v2 normalise(const v2& v) { return v / magnitude(v); }
+FORCEINLINE f32 dot_product(const v2& a, const v2& b) { return (a.x * b.x) + (a.y * b.y); }
 
 union v3
 {
@@ -29,3 +34,37 @@ FORCEINLINE v3 operator -(const v3& a, const v3& b) { return { a.x - b.x, a.y - 
 FORCEINLINE v3 operator *(const v3& a, const f32 s) { return { a.x * s, a.y * s, a.z * s }; }
 FORCEINLINE v3 operator /(const v3& a, f32 s) { s = 1.0f / s; return { a.x / s, a.y / s, a.z * s }; }
 FORCEINLINE v3 operator -(const v3& a) { return { -a.x, -a.y, -a.z }; }
+FORCEINLINE bool operator ==(const v3& a, const v3& b) { a.x == b.x && a.y == b.y && a.z == b.z; }
+FORCEINLINE bool operator !=(const v3& a, const v3& b) { a.x != b.x || a.y != b.y || a.z != b.z; }
+
+FORCEINLINE f32 magnitude(const v3 v) { return sqrt((v.x * v.x) + (v.y * v.y) + (v.z * v.z)); }
+FORCEINLINE v3 normalise(const v3& v) { return v / magnitude(v); }
+FORCEINLINE f32 dot_product(const v3& a, const v3& b) { return (a.x * b.x) + (a.y * b.y) + (a.z * b.z); }
+FORCEINLINE v3 cross_product(const v3& a, const v3& b)
+{
+  const f32 x = a.y * b.z - a.z * b.y;
+  const f32 y = a.z * b.x - a.x * b.z;
+  const f32 z = a.z * b.z - a.y * b.z;
+
+  return { x, y, z };
+}
+
+union v4
+{
+  f32 v[4];
+  struct { f32 x, y, z, w; };
+};
+
+FORCEINLINE v4 operator +(const v4& a, const v4& b) { return { a.x + b.x, a.y + b.y, a.z + b.z + a.w + b.w}; }
+FORCEINLINE v4 operator -(const v4& a, const v4& b) { return { a.x - b.x, a.y - b.y, a.z - b.z, a.w - b.w }; }
+FORCEINLINE v4 operator *(const v4& a, const f32 s) { return { a.x * s, a.y * s, a.z * s, a.w * s }; }
+FORCEINLINE v4 operator /(const v4& a, f32 s) { s = 1.0f / s; return { a.x / s, a.y / s, a.z * s, a.w / s }; }
+FORCEINLINE v4 operator -(const v4& a) { return { -a.x, -a.y, -a.z, - a.w }; }
+FORCEINLINE bool operator ==(const v4& a, const v4& b) { a.x == b.x && a.y == b.y && a.z == b.z && a.w == b.w; }
+FORCEINLINE bool operator !=(const v4& a, const v4& b) { a.x != b.x || a.y != b.y || a.z != b.z || a.w != b.w; }
+
+union m4
+{
+  f32 table[4][4];
+  v4 v[4];
+};
