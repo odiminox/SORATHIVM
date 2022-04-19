4e7380ae52a3e06ef78a93ad072f9f224e12ad76
Simon Jordan <simon@thepolygonloft.com>
2022-04-19 18:54:17 +0100

    fix tabs to spaces

--- blueprint/source/graphics.h
+++ blueprint/source/graphics.h
@@ -4,63 +4,63 @@
 
 struct Uv
 {
-	v2 data;
+    v2 data;
 };
 
 struct Material
 {
-	STRING name;
-	u32 id;
+    STRING name;
+    u32 id;
 };
 
 struct Texture
 {
-	u32 id;
-	i32 width;
-	i32 height;
+    u32 id;
+    i32 width;
+    i32 height;
 };
 
 struct Vertex
 {
-	v4 colour;
-	v3 pos;
-	v3 normal;
+    v4 colour;
+    v3 pos;
+    v3 normal;
 };
 
 
 struct Edge
 {
-	Vertex vert1;
-	Vertex vert2;
+    Vertex vert1;
+    Vertex vert2;
 };
 
 struct Poly
 {
-	Edge edges[3];
-	v3 pos;
-	Uv uv;
+    Edge edges[3];
+    v3 pos;
+    Uv uv;
 };
 
 struct Particle
 {
-	union
-	{
-		v4 start_colour;
-		v4 colour;
-	};
+    union
+    {
+        v4 start_colour;
+        v4 colour;
+    };
 
-	v4 end_colour;
+    v4 end_colour;
 
-	v3 pos;
-	v3 speed;
+    v3 pos;
+    v3 speed;
 };
 
 struct Mesh
 {
-	m4 trans;
-	Poly* polygon_list;
-	Vertex* vertex_list;
-	Edge* edge_list;
-	Material* mat;
-	u16 id;
+    m4 trans;
+    Poly* polygon_list;
+    Vertex* vertex_list;
+    Edge* edge_list;
+    Material* mat;
+    u16 id;
 };
