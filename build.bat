6507302cd1f5488cca0c8b1a2e5fb18a1bbfadc3
Simon Jordan <simon@thepolygonloft.com>
2022-04-22 22:28:53 +0100

    optimised flags for build script

--- build.bat
+++ build.bat
@@ -2,5 +2,5 @@
 
 mkdir ..\build
 pushd ..\build
-cl /Zi ..\blueprint\blueprint\source\*.cpp  user32.lib gdi32.lib opengl32.lib
+cl /Zi ..\blueprint\blueprint\source\*.cpp /GL /Os /GF /Gy /GA /O1 /GX /Os /Og /D NODEFAULTLIB=0 /D _ITERATOR_DEBUG_LEVEL=0 user32.lib gdi32.lib opengl32.lib
 popd
